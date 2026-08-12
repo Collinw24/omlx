9f0f2152f2340e5f52c2f340159806584b137bb1
Collinw24 <collin@luxiumdigital.net>
2026-08-12 16:53:02 -0400

    fix(turboquant): harden conversion safety and preflight

--- omlx/admin/static/js/dashboard.js
+++ omlx/admin/static/js/dashboard.js
@@ -6570,8 +6570,7 @@
                         continue;
                     }
                     if (k === 'turboquant_mid_prefill') {
-                        out.turboquant_mid_prefill = !!ms.turboquant_kv_enabled
-                            && !!ms.turboquant_mid_prefill;
+                        out.turboquant_mid_prefill = !!ms.turboquant_mid_prefill;
                         continue;
                     }
                     if (k === 'guided_grammar_enabled') {
@@ -7480,8 +7479,7 @@
                                 forced_ct_kwargs: forcedCtKwargs.length > 0
                                     ? forcedCtKwargs : null,
                                 turboquant_kv_enabled: this.modelSettings.turboquant_kv_enabled,
-                                turboquant_mid_prefill: this.modelSettings.turboquant_kv_enabled
-                                    && !!this.modelSettings.turboquant_mid_prefill,
+                                turboquant_mid_prefill: !!this.modelSettings.turboquant_mid_prefill,
                                 turboquant_kv_bits: this.modelSettings.turboquant_kv_enabled
                                     ? (parseFloat(this.modelSettings.turboquant_kv_bits) || 4)
                                     : 4,
